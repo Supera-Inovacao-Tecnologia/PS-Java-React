98004c35db732089c3fd94ea3a83ef5dc9201290
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-11 18:03:24 -0300

    @Service -> ContaService created

---
+++ src/main/java/br/com/banco/services/ContaService.java
@@ -0,0 +1,13 @@
+package br.com.banco.services;
+
+import br.com.banco.repositories.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+}
