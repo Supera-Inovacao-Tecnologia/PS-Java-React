39adc40e928854434b0e6aff1cef7d06dff26781
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-11 18:01:42 -0300

    @Repository -> ContaRepository implementation

---
+++ src/main/java/br/com/banco/repositories/ContaRepository.java
@@ -0,0 +1,9 @@
+package br.com.banco.repositories;
+
+import br.com.banco.model.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta,Long> {
+}
