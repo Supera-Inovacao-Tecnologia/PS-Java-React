a83a4509c81b30fd67e50039c2e1cc7c6a3dd0a3
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-11 18:01:19 -0300

    @Entity -> Conta.java, @GeneratedValue

--- src/main/java/br/com/banco/model/Conta.java
+++ src/main/java/br/com/banco/model/Conta.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Conta  implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_conta")
     private long id;
 
