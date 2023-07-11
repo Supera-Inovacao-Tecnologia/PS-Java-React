37319911a3d2c374cabb4c26c70474b8360a09b1
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-11 18:01:49 -0300

    @Repository -> TransferenciaRepository implementation

---
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -0,0 +1,9 @@
+package br.com.banco.repositories;
+
+import br.com.banco.model.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferenciaRepository extends JpaRepository<Transferencia,Long> {
+}
