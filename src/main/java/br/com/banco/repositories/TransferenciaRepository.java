5ac2db4de56a09e5bc47231b11bccb889a44cf16
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-12 19:30:14 -0300

    @Repository -> QuerydslPredicateExecutor extends into TransferenciaRepository

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -2,8 +2,9 @@ package br.com.banco.repositories;
 
 import br.com.banco.model.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransferenciaRepository extends JpaRepository<Transferencia,Long> {
+public interface TransferenciaRepository extends JpaRepository<Transferencia,Long>, QuerydslPredicateExecutor<Transferencia> {
 }
