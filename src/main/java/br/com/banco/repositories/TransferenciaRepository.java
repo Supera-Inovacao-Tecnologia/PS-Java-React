9c83bf4e48245f198a9db81d7d7b5fa22bbd69eb
Francine Hahn <fran_hahn@hotmail.com>
2023-07-13 18:33:06 -0300

    refactor: I deleted an import that was not being used

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Integer> {
