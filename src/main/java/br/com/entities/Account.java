a0189619bd7b6f49c0924b938b326aa68cce6c09
Francine Hahn <fran_hahn@hotmail.com>
2023-07-13 08:49:11 -0300

    chore: I started writing the class Account

---
+++ src/main/java/br/com/entities/Account.java
@@ -0,0 +1,13 @@
+package br.com.entities;
+
+public class Account {
+    private int accountId;
+    private String clientName;
+
+    public int getAccountId() {
+        return accountId;
+    }
+    public String getClientName() {
+        return clientName;
+    } 
+}
