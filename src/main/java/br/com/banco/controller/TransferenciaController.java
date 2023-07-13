9cdd5329e6e085172fb4bf5ddc07bda610778799
Francine Hahn <fran_hahn@hotmail.com>
2023-07-13 19:07:35 -0300

    refactor: I put all the error handling in the service layer instead of the controller layer

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -2,9 +2,6 @@ package br.com.banco.controller;
 
 import br.com.banco.services.TransferenciaService;
 import br.com.banco.entities.Transferencia;
-import br.com.banco.exception.FiltroNaoEncontradoException;
-import br.com.banco.exception.ParametroDeTempoException;
-import br.com.banco.exception.ParametrosInvalidosException;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -30,8 +24,6 @@ public class TransferenciaController {
         this.service = service;
     }
 
-    private static final List<String> parametrosEsperados = Arrays.asList("contaId", "nomeOperador", "mes", "ano");
-
     @GetMapping
     public List<Transferencia> buscarTransferencias(
         @RequestParam(required = false) Integer contaId, 
@@ -40,25 +32,6 @@ public class TransferenciaController {
         @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy") Integer ano,
         HttpServletRequest request
     ) {
-        List<String> parametrosRecebidos = Collections.list(request.getParameterNames());
-        parametrosRecebidos.removeAll(parametrosEsperados);
-
-        if (!parametrosRecebidos.isEmpty()) {
-            throw new FiltroNaoEncontradoException("Parâmetros inválidos fornecidos na requisição: " + parametrosRecebidos);
-        }
-
-        if ((mes == null && ano != null) || mes != null && ano == null) {
-            throw new ParametroDeTempoException("Você não pode passar como parâmetro apenas o mês ou o ano.");
-        }
-
-        if (contaId != null && nomeOperador != null && mes == null && ano == null) {
-            throw new ParametrosInvalidosException("Você não pode passar o id da conta e o nome do operador como parâmetros. Os dois só podem ser passados em conjunto se o mês e o ano forem informados também.");
-        }
-
-        if (contaId != null && mes != null && ano != null && nomeOperador == null) {
-            throw new ParametrosInvalidosException("Você não pode passar o id da conta e o mês e o ano como parâmetros. Eles só podem ser passados em conjunto se o nome do operador for informado também.");
-        }
-
-        return service.buscarTransferencias(contaId, nomeOperador, mes, ano);
+        return service.buscarTransferencias(contaId, nomeOperador, mes, ano, request);
     }
 }
--- src/main/java/br/com/banco/services/TransferenciaService.java
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -1,13 +1,17 @@
 package br.com.banco.services;
 
 import br.com.banco.entities.Transferencia;
+import br.com.banco.exception.FiltroNaoEncontradoException;
+import br.com.banco.exception.ParametroDeTempoException;
+import br.com.banco.exception.ParametrosInvalidosException;
 import br.com.banco.repositories.TransferenciaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
 
 @Service
 public class TransferenciaService {
@@ -18,7 +22,31 @@ public class TransferenciaService {
       this.repository = repository;
   }
 
-  public List<Transferencia> buscarTransferencias(Integer contaId, String nomeOperador, Integer mes, Integer ano) {
+  public List<Transferencia> buscarTransferencias(
+    Integer contaId, String nomeOperador, Integer mes, Integer ano, HttpServletRequest request
+  ) {
+
+    List<String> parametrosEsperados = Arrays.asList("contaId", "nomeOperador", "mes", "ano");
+    List<String> parametrosRecebidos = Collections.list(request.getParameterNames());
+    
+    parametrosRecebidos.removeAll(parametrosEsperados);
+
+    if (!parametrosRecebidos.isEmpty()) {
+        throw new FiltroNaoEncontradoException("Parâmetros inválidos fornecidos na requisição: " + parametrosRecebidos);
+    }
+
+    if ((mes == null && ano != null) || mes != null && ano == null) {
+        throw new ParametroDeTempoException("Você não pode passar como parâmetro apenas o mês ou o ano.");
+    }
+
+    if (contaId != null && nomeOperador != null && mes == null && ano == null) {
+        throw new ParametrosInvalidosException("Você não pode passar o id da conta e o nome do operador como parâmetros. Os dois só podem ser passados em conjunto se o mês e o ano forem informados também.");
+    }
+
+    if (contaId != null && mes != null && ano != null && nomeOperador == null) {
+        throw new ParametrosInvalidosException("Você não pode passar o id da conta e o mês e o ano como parâmetros. Eles só podem ser passados em conjunto se o nome do operador for informado também.");
+    }
+
     if (nomeOperador != null && mes != null & ano != null) {
       return repository.buscarTransferenciasPorMesAnoEoperador(nomeOperador, mes, ano);
     } else if (contaId != null) {
