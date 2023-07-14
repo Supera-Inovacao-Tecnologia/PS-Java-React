11b3674ae06b4dc64b4ac5cd5192c75d15997f95
Francine Hahn <fran_hahn@hotmail.com>
2023-07-14 10:25:36 -0300

    refactor: I excluded some imports that were not being used

--- src/test/java/br/com/banco/TransferenciaRepositoryTest.java
+++ src/test/java/br/com/banco/TransferenciaRepositoryTest.java
@@ -1,14 +1,11 @@
 package br.com.banco;
 
-import br.com.banco.entities.Conta;
 import br.com.banco.entities.Transferencia;
 import br.com.banco.repositories.TransferenciaRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
