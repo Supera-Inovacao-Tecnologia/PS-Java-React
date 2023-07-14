36ca47ad024833602efc86c4229cc54ba59398ca
Francine Hahn <fran_hahn@hotmail.com>
2023-07-13 23:25:53 -0300

    feat: I implemented the buscarTransferencias_PorConta_DeveRetornarTransferenciasCorretas test

--- src/test/java/br/com/banco/TransferenciaServiceTest.java
+++ src/test/java/br/com/banco/TransferenciaServiceTest.java
@@ -16,8 +16,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -37,6 +35,29 @@ class TransferenciaServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    Conta conta1 = new Conta(1, "Fulano");
+    Transferencia transferencia1 = new Transferencia(
+        1, 
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-01 12:00:00+03", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))), 
+        100.40, 
+        "DEPÓSITO", 
+        null,
+        conta1
+    );
+
+    Conta conta2 = new Conta(2, "Sicrano");
+    Transferencia transferencia2 = new Transferencia(
+        2, 
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-21 12:00:00+03", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))), 
+        200.50, 
+        "Transferência",
+        "Beltrano",
+        conta2
+    );
+
+    List<Transferencia> transferenciasEsperadas = Arrays.asList(transferencia1, transferencia2);
+
+
     @Test
     public void buscarTransferencias_Todas_DeveRetornarTransferenciasCorretas() {
         
@@ -44,52 +65,12 @@ class TransferenciaServiceTest {
 
     @Test
     public void buscarTransferencias_PorConta_DeveRetornarTransferenciasCorretas() {
-        /*Conta conta = new Conta(1, "Sicrano");
+        when(repository.buscarTransferenciasPorConta(1)).thenReturn(Arrays.asList(transferencia1));
+        List<Transferencia> transferencias = service.buscarTransferencias(1, null, null, null);
 
-        List<Transferencia> transferenciasEsperadas = Arrays.asList(
-                new Transferencia(
-                    1, 
-                    Timestamp.valueOf(LocalDateTime.parse("2023-01-01 12:00:00+03", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))), 
-                    100.40, 
-                    "DEPÓSITO", 
-                    null,
-                    conta
-                ),
-                new Transferencia(
-                    2, 
-                    Timestamp.valueOf(LocalDateTime.parse("2023-01-01 12:00:00+03", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))), 
-                    200.50, 
-                    "Transferência",
-                    "Beltrano",
-                    conta
-                )
-        );
-
-        when(repository.buscarTransferenciasPorConta(1)).thenReturn(transferenciasEsperadas);
-
-        // Chamada do método para buscar transferências por conta
-        List<Transferencia> transferencias = service.buscarTransferenciasPorConta(1, null, null, null, );
-
-        // Verificação dos resultados esperados
-        assertEquals(2, transferencias.size());
+        assertEquals(1, transferencias.size());
         assertEquals(transferencia1, transferencias.get(0));
-        assertEquals(transferencia2, transferencias.get(1));
-
-        // Verificação se as transferências retornadas correspondem à conta esperada
-        verify(repository, times(1)).findByContaId(1);
-        assertEquals(2, transferencias.size());
-
-        Transferencia transferencia1 = transferencias.get(0);
-        assertEquals(1, transferencia1.getId());
-        assertEquals(100.0, transferencia1.getValor());
-        assertEquals("Transferência", transferencia1.getTipo());
-        assertEquals(1, transferencia1.getContaId());
-
-        Transferencia transferencia2 = transferencias.get(1);
-        assertEquals(2, transferencia2.getId());
-        assertEquals(200.0, transferencia2.getValor());
-        assertEquals("Transferência", transferencia2.getTipo());
-        assertEquals(1, transferencia2.getContaId());*/
+        verify(repository, times(1)).buscarTransferenciasPorConta(1);
     }
 
     @Test
