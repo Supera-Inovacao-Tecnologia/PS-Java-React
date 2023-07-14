ff4dd4f2863bdd956adac7919530f6d64eb8feb4
igor <quaresmaigor@yahoo.com>
2023-07-14 13:28:18 -0300

    test

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -8,6 +8,7 @@ public class BancoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BancoApplication.class, args);
+        System.out.println("teste");
     }
 
 }
