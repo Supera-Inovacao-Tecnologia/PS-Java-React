53f856c91fee026ba987dad9fc5f018ca281c809
Francine Hahn <fran_hahn@hotmail.com>
2023-07-14 09:41:23 -0300

    feat: I implemented the testarBuscarTodasTransferenciasPorIdConta test

--- src/test/java/br/com/banco/TransferenciaRepositoryTest.java
+++ src/test/java/br/com/banco/TransferenciaRepositoryTest.java
@@ -1,9 +1,9 @@
 package br.com.banco;
 
+import br.com.banco.entities.Conta;
 import br.com.banco.entities.Transferencia;
 import br.com.banco.repositories.TransferenciaRepository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -29,6 +28,16 @@ public class TransferenciaRepositoryTest {
     @Test
     public void testarBuscarTodasAsTransferencias() {
         List<Transferencia> resposta = repository.buscarTodasTransferencias();
+
+        assertNotNull(resposta);
+        assertTrue(resposta.stream().anyMatch(res -> res.getTipo().contains("DEPOSITO")));
+        assertTrue(resposta.stream().anyMatch(res -> res.getTipo().contains("SAQUE")));
+        assertTrue(resposta.stream().anyMatch(res -> res.getTipo().contains("TRANSFERENCIA")));
+    }
+
+    @Test
+    public void testarBuscarTodasTransferenciasPorIdConta() {
+        List<Transferencia> resposta = repository.buscarTransferenciasPorConta(1);
 
         assertNotNull(resposta);
         assertTrue(resposta.stream().anyMatch(res -> res.getTipo().contains("DEPOSITO")));
