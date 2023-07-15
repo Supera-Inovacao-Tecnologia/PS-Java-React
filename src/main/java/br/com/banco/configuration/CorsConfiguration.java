079fb31de1cfbf86dc4d4eda974211e6d60f3af7
leandroleiteh <leandroluz201616@gmail.com>
2023-07-14 21:23:36 -0300

    Adicionado arquivo de configuração de CrosOrigin

---
+++ src/main/java/br/com/banco/configuration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.banco.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
